e522c749af401c2b7445ed5d0127d260e5674f8e
javier <jgrana@herrera.unt.edu.ar>
2023-09-05 19:54:09 -0300

    Primer Web API - 02 Get Productos

---
+++ Clase 3/PrimerWEBAPI/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerWEBAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductosController : ControllerBase
+{
+     private static List<Producto> productos = new List<Producto>
+        {
+            new Producto { Id = 1, Nombre = "Arroz", PrecioEnPesos = 50.99M },
+            new Producto { Id = 2, Nombre = "Leche", PrecioEnPesos = 30.49M },
+            new Producto { Id = 3, Nombre = "Pan", PrecioEnPesos = 25.00M },
+            new Producto { Id = 4, Nombre = "Huevos", PrecioEnPesos = 40.75M },
+            new Producto { Id = 5, Nombre = "Aceite de cocina", PrecioEnPesos = 70.25M }
+        };
+
+    private readonly ILogger<ProductosController> _logger;
+
+    public ProductosController(ILogger<ProductosController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetProductos")]
+    [Route("GetProductos")]
+    public IEnumerable<Producto> Get()
+    {
+        return productos.ToArray();
+    }
+}
---
+++ Clase 3/PrimerWEBAPI/Modelos/Producto.cs
@@ -0,0 +1,8 @@
+namespace PrimerWEBAPI;
+
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public decimal PrecioEnPesos { get; set; }
+}
