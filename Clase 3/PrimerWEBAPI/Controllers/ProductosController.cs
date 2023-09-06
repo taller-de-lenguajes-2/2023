088e442391d60566f5b120abc241c447e24a769f
javier <jgrana@herrera.unt.edu.ar>
2023-09-06 00:37:46 -0300

    Primer Web API - 02 Get Producto con dos parámetros

--- Clase 3/PrimerWEBAPI/Controllers/ProductosController.cs
+++ Clase 3/PrimerWEBAPI/Controllers/ProductosController.cs
@@ -9,11 +9,11 @@ public class ProductosController : ControllerBase
 {
      private static List<Producto> productos = new List<Producto>
         {
-            new Producto { Id = 1, Nombre = "Arroz", PrecioEnPesos = 50.99M },
-            new Producto { Id = 2, Nombre = "Leche", PrecioEnPesos = 30.49M },
-            new Producto { Id = 3, Nombre = "Pan", PrecioEnPesos = 25.00M },
-            new Producto { Id = 4, Nombre = "Huevos", PrecioEnPesos = 40.75M },
-            new Producto { Id = 5, Nombre = "Aceite de cocina", PrecioEnPesos = 70.25M }
+            new Producto { Id = 1, Nombre = "Arroz", PrecioEnPesos = 50.99M, Stock = 10 },
+            new Producto { Id = 2, Nombre = "Leche", PrecioEnPesos = 30.49M, Stock = 20 },
+            new Producto { Id = 3, Nombre = "Pan", PrecioEnPesos = 25.00M, Stock= 0 },
+            new Producto { Id = 4, Nombre = "Huevos", PrecioEnPesos = 40.75M, Stock = 100},
+            new Producto { Id = 5, Nombre = "Aceite de cocina", PrecioEnPesos = 70.25M, Stock = 1 }
         };
 
     private readonly ILogger<ProductosController> _logger;
@@ -29,14 +29,24 @@ public class ProductosController : ControllerBase
         return Ok(productos);
     }
 
-    [HttpGet("Producto/{id}")]   
+    [HttpGet("GetProducto/{id}")]   
     public ActionResult<Producto> Get(int id)
     {
         Producto producto = productos.FirstOrDefault( a=> a.Id == id);
         
         if(producto==null) return NotFound();
         
-        return Ok();
+        return Ok(producto);
+    }
+
+    [HttpGet("GetProducto/{id}/{stock}")]   
+    public ActionResult<Producto> Get(int id, int stock)
+    {
+        Producto producto = productos.FirstOrDefault( a=> a.Id == id && a.Stock > stock );
+        
+        if(producto==null) return NotFound();
+        
+        return Ok(producto);
     }
 
     
--- Clase 3/PrimerWEBAPI/Modelos/Producto.cs
+++ Clase 3/PrimerWEBAPI/Modelos/Producto.cs
@@ -5,4 +5,5 @@ public class Producto
     public int Id { get; set; }
     public string Nombre { get; set; }
     public decimal PrecioEnPesos { get; set; }
+    public int Stock { get; set; }
 }
