b9b2d6e3677f2984892eb25b8eb97565e7bdd55a
javier <jgrana@herrera.unt.edu.ar>
2023-11-10 16:39:51 -0300

    Se agregó un ejemplo de proyecto MVC

---
+++ MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PruebaMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ MVC/Models/Producto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace PruebaMVC.Models
+{
+    public class Producto
+    {
+        public int Id {get;set;}
+        public string Nombre {get;set;}
+        public int Precio {get;set;}
+    }
+}
